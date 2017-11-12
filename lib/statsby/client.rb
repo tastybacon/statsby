455ed7809344cb9f0daebc3875b7e33bac8f7c35
tastybacon <tastycuredpork@gmail.com>
2017-11-12 17:00:47 -0500

    Make Statsby::Client taggable

--- lib/statsby/client.rb
+++ lib/statsby/client.rb
@@ -6,17 +6,23 @@ module Statsby
     DEFAULT_HOST = 'localhost'.freeze
     DEFAULT_PORT = 8125
 
-    attr_reader :socket, :host, :port, :tags_enabled
+    attr_reader :socket, :host, :port, :tags, :tags_enabled
 
-    def initialize(host: DEFAULT_HOST, port: DEFAULT_PORT, tags_enabled: true)
+    def initialize(
+      host: DEFAULT_HOST,
+      port: DEFAULT_PORT,
+      tags: {},
+      tags_enabled: true
+    )
       @socket = UDPSocket.new
       @host = host
       @port = port
+      @tags = Statsby::TagSet.from_hash(tags)
       @tags_enabled = tags_enabled
     end
 
-    def send_message(metric_name, value, type, tags = {})
-      message = "#{metric_name}#{format_tags(tags)}:#{value}|#{type}"
+    def send_message(metric_name, value, type, message_tags = {})
+      message = "#{metric_name}#{format_tags(message_tags)}:#{value}|#{type}"
       puts "Sending #{message}"
       socket.send(message, 0, host, port)
     end
@@ -37,8 +43,9 @@ module Statsby
       send_message(metric_name, value, 's')
     end
 
-    def format_tags(tags)
-      ",#{Statsby::TagSet.from_hash(tags)}" if tags_enabled
+    def format_tags(message_tags = {})
+      combined_tags = tags.merge(message_tags)
+      ",#{combined_tags}" if tags_enabled && !combined_tags.empty?
     end
   end
 end
--- spec/lib/statsby/client_spec.rb
+++ spec/lib/statsby/client_spec.rb
@@ -2,4 +2,30 @@ require 'statsby'
 RSpec.describe Statsby::Client do
   let(:default_client) { Statsby::Client.new }
   let(:tagless_client) { Statsby::Client.new(tags_enabled: false) }
+  let(:tagged_client) do
+    Statsby::Client.new(tags: { 'key' => 'value', 'key2' => 'value2' })
+  end
+
+  describe '#format_tags' do
+    it 'returns nil when tags are disabled' do
+      expect(tagless_client.format_tags('test' => 'tag')).to be_nil
+    end
+
+    it 'returns nil when tags are empty' do
+      expect(default_client.format_tags).to be_nil
+    end
+
+    it 'uses client based tags' do
+      expect(tagged_client.format_tags).to eq ',key=value,key2=value2'
+    end
+
+    it 'uses message based tags' do
+      expect(default_client.format_tags('test' => 'tag')).to eq ',test=tag'
+    end
+
+    it 'overwrites client tags with message tags' do
+      expect(tagged_client.format_tags('key' => 'new_value'))
+        .to eq ',key=new_value,key2=value2'
+    end
+  end
 end
