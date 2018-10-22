f903ff17060164cd695c262ad986a503ff0c85e9
tastybacon <tastycuredpork@gmail.com>
2018-10-21 23:29:47 -0400

    Send the correct type from sub contexts

--- lib/statsby/context.rb
+++ lib/statsby/context.rb
@@ -1,46 +1,52 @@
+require 'forwardable'
+
 require 'statsby/tag_set'
 module Statsby
   # This is meant to be used as a thin layer over a
   # client or another context as a way to organize tags.
   class Context
-    attr_reader :super_context, :tags
+    attr_reader :client, :tags
 
-    def initialize(super_context, tags = {})
-      @super_context = super_context
-      @tags = Statsby::TagSet.from_hash(tags)
+    def initialize(client, tags = {})
+      self.client = client
+      self.tags = Statsby::TagSet.from_hash(tags)
     end
 
     def counter(metric_name, value, local_tags = {})
       combined_tags = tags.merge(local_tags)
-      super_context.counter(metric_name, value, combined_tags)
+      client.counter(metric_name, value, combined_tags)
     end
 
     def gauge(metric_name, value, local_tags = {})
       combined_tags = tags.merge(local_tags)
-      super_context.counter(metric_name, value, combined_tags)
+      client.gauge(metric_name, value, combined_tags)
     end
 
     def timing(metric_name, value, local_tags = {})
       combined_tags = tags.merge(local_tags)
-      super_context.counter(metric_name, value, combined_tags)
+      client.timing(metric_name, value, combined_tags)
     end
 
     def set(metric_name, value, local_tags = {})
       combined_tags = tags.merge(local_tags)
-      super_context.counter(metric_name, value, combined_tags)
+      client.set(metric_name, value, combined_tags)
     end
 
     def format_tags(message_tags = {})
-      super_context.format_tags(tags.merge(message_tags))
+      client.format_tags(tags.merge(message_tags))
     end
 
     def format_message(metric_name, value, type, message_tags = {})
       combined_tags = tags.merge(message_tags)
-      super_context.format_message(metric_name, value, type, combined_tags)
+      client.format_message(metric_name, value, type, combined_tags)
     end
 
     def subcontext(tags = {})
       Statsby::Context.new(self, tags)
     end
+
+    private
+
+    attr_writer :client, :tags
   end
 end
